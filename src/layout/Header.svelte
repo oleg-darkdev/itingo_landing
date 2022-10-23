a5fba08f0e79413f35a7f996602d36227110e979
DarkDev <darkdev_brand@riseup.net>
2022-10-23 12:27:56 +0300

    add Header

---
+++ src/layout/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte'
+</script>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+  </NavBrand>
+  <div class="flex items-center md:order-2">
+    <Avatar id="avatar-menu" src="/images/profile-picture-3.webp" />
+    <NavHamburger on:click={toggle} class1="w-full md:flex md:w-auto md:order-1"/>
+  </div>
+  <Dropdown placement="bottom" triggeredBy="#avatar-menu">
+    <DropdownHeader>
+    <span class="block text-sm"> Bonnie Green </span>
+    <span class="block truncate text-sm font-medium"> name@flowbite.com </span>
+    </DropdownHeader>
+    <DropdownItem>Dashboard</DropdownItem>
+    <DropdownItem>Settings</DropdownItem>
+    <DropdownItem>Earnings</DropdownItem>
+    <DropdownDivider />
+    <DropdownItem>Sign out</DropdownItem>
+  </Dropdown>
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/services">Services</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Header from '../layout/Header.svelte';
+</script>
+
+<Header />
+<slot />
+
+
+<style>
+	:global(section) {
+		margin-bottom: 30px;
+	}
+	:global(.justify-center, section)  {
+        display: flex;
+        justify-content: center;
+    }
+    :global(.row) {
+        display: flex;
+        flex-direction: row;
+    }
+	:global(column) {
+        display: flex;
+		flex-direction: column;
+	}
+
+    :global(.bg) {
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+	:global(body, html) {
+		height: 100%;
+		margin: 0;
+		padding: 0;
+	}
+</style>
