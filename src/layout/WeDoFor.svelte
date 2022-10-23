b2744e0ec73161245331a49d54abfb3c5875d8f0
DarkDev <darkdev_brand@riseup.net>
2022-10-23 13:43:33 +0300

    intial WeDoFor section

---
+++ src/layout/WeDoFor.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { ButtonGroup, Button } from "flowbite-svelte";
+</script>
+
+
+
+<div class="justify-center" style="margin-bottom: -150px;">
+<div class="text-center flex max-w-2xl flex-col bg-viol shadow-md text-gray-500 dark:text-gray-400 rounded-lg  p-8 text-center">
+  <h5 class="mb-2 text-3xl font-bold text-white">Wspieramy wsperajiencych</h5>
+  <p class="mb-5 text-base text-white sm:text-lg dark:text-gray-400">
+    Vollie is an online marketplace that connects skilled people to non-profits, charities and social enterprises for skills-based online volunteering. Vollie projects are exclusively online, meaning that people can donate their skills and experience from anywhere in the world, and around their busy personal and professional schedule. Sign up today and start changing the world!
+  </p>
+  <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+		<ButtonGroup>
+			<Button outline size="lg" color="yellow">For Startups</Button>
+			<Button outline  size="lg" color="yellow">For Volunteers</Button>
+			<Button outline size="lg" color="yellow">For NGO`s</Button>
+			<Button outline ="lg" color="yellow">For business</Button>
+		</ButtonGroup>
+  </div>
+</div>
+</div>
