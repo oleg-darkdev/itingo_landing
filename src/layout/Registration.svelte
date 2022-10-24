78b7b1768a42f5cd7da672b09cf378c716c88a3c
DarkDev <darkdev_brand@riseup.net>
2022-10-24 15:48:45 +0300

    alfa v. steps registration section

---
+++ src/layout/Registration.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Card, ButtonGroup, Button, Toggle } from "flowbite-svelte";
+</script>
+
+<div class="w-full flex justify-center ">
+<Card style="" class="mb-32 " img="/images/landing/registration.svg" horizontal reverse={false}>
+    <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-violDark dark:text-white">Łatwy start do projektów</h5>
+    <p class="mb-3 font-normal text-center  text-viol dark:text-gray-400 leading-tight">
+      ITingo to platforma internetowa, która łączy IT, wolontariuszy, organizacje pozarządowe, biznes i start-upy.
+    </p>
+	<div class="w-full mt-4 mb-2 flex  justify-center ">
+        <div class="w-8/12 flex flex-col ">
+            <Button size="md" class="mb-1" color="yellow">Dla IT wolontariutu</Button>
+            <Button size="md" class="mb-1" color="yellow">Dla NGO</Button>
+            <Button size="md" class="mb-1" color="yellow">Dla biznesu</Button>
+            <Button size="md" color="yellow">Dla startupow</Button>
+        </div>
+	</div>
+</Card>
+</div>
+
