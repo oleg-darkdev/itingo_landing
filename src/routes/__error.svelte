54398e30c933de1dbaf1253ccd5e2e343c37eef8
DarkDev <darkdev_brand@riseup.net>
2022-10-24 16:54:30 +0300

    alfa v. 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<div class="not-found w-full">
+    <div style="height: 450px;" class="w-full flex flex-col items-center">
+	<h1 style="font-size: 300px;" class="m-0 p-0 text-center  text-lightYellow font-bold ">404</h1>
+    <p style="font-size: 90px; margin-top: -100px; width: 50%; min-width: 350px;" class="mb-3 text-center font-normal text-lightYellow">Nie znaleziono strony</p>
+    </div>
+</div>
+
+
+<style>
+    .not-found {
+		min-height: 100vh;
+		height: auto;
+		background-image: url(/images/landing/404-bg.svg);
+		background-repeat: no-repeat;
+		background-size: 90%; 
+		background-attachment: fixed;
+		background-position: center;
+	}
+</style>
