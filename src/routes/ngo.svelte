71a5f08ead2843487d49cd2e5ae40d3c2df4b727
DarkDev <darkdev_brand@riseup.net>
2022-10-24 18:35:32 +0300

    basic page structure

---
+++ src/routes/ngo.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { Button  } from "flowbite-svelte";
+	import HelloSection from '../layout/landing/HelloSection.svelte';
+	import StepsSection from '../layout/landing/StepsSection.svelte';
+	import ImpactSection from '../layout/landing/ImpactSection.svelte';
+	import OurFamilySection from '../layout/landing/OurFamilySection.svelte';
+	import FaqSection from '../layout/landing/FaqSection.svelte';
+	import ContactSection from '../layout/landing/ContactSection.svelte';
+
+	import TypesOfWorks from '../layout/ngo/TypesOfWorks.svelte';
+
+	
+
+
+</script>
+
+<svelte:head>
+	<title>ITingo dla NGO</title>
+</svelte:head>
+
+
+
+<HelloSection>
+	<Button outline size="lg" color="purple">Znajdź wolontariuszy</Button>
+</HelloSection>
+
+<TypesOfWorks />
+
+<StepsSection />
+
+<ImpactSection />
+
+<OurFamilySection />
+
+<FaqSection />
+
+<ContactSection />
