df4da9f2afb6432ef7d0be279596bf0488ef8c47
DarkDev <darkdev_brand@riseup.net>
2022-10-25 14:11:49 +0300

    add block for volounteers, ngo - update anchors, links,

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -3,13 +3,123 @@
 </script>
 
 <div class="grid grid-cols-2 gap-8 py-8 px-6 md:grid-cols-4 bg-viol">
+
   <div>
-    <h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase">O nas
+    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Dla NGO
     </h2> 
-       
+    <ul class="text-gray-300">
+      <li class="mb-4">
+        <a href="/ngo#volunteer" class="hover:underline">Znajdź IT wolontariusza
+        </a>
+      </li>
+      <!--<FooterLink>--> 
+      <li class="mb-4">
+        <a href="/ngo#faq " class="hover:underline">FAQ
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo#design" class="hover:underline">Pomóc z graficami
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo#projecting" class="hover:underline">Pomóc z projektowaniem stron
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo#security" class="hover:underline">Pomóc z bezpieczeństwom IT
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo#seo" class="hover:underline">Pomóc z Seo
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo#server" class="hover:underline">Pomóc na serwerze
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo#site" class="hover:underline">Pomóc z tworzeniem strony
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo#social-media" class="hover:underline">Pomoc w zakresie mediów 
+        </a>
+      </li>
+      <!--<FooterLink>--> 
+
+      <!--<FooterLink>-->
+    </ul>
+    <!--<FooterLinkGroup>-->
+  </div> <div>
+    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Dla IT wolontariutu
+    </h2> 
+    <ul class="text-gray-300">
+      <li class="mb-4">
+        <a href="/volunteer#start" class="hover:underline">Zostań IT wolontariuszem
+        </a>
+      </li>
+      <!--<FooterLink>--> 
+      <li class="mb-4">
+        <a href="/volunteer#faq " class="hover:underline">FAQ
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer#design" class="hover:underline">Pomóc jako grafica
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer#projecting" class="hover:underline">Pomóc z projektowaniem stron
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer#security" class="hover:underline">Pomóc z bezpieczeństwom IT
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer#seo" class="hover:underline">Pomóc z Seo
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer#server" class="hover:underline">Pomóc na serwerze
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer#site" class="hover:underline">Pomóc z tworzeniem strony
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer#social-media" class="hover:underline">Pomoc w zakresie mediów 
+        </a>
+      </li>
+      <!--<FooterLink>--> 
+
+      <!--<FooterLink>-->
+    </ul>
+    <!--<FooterLinkGroup>-->
+  </div> 
+  <div>
+    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Wspieramy wsperajiencych
+    </h2> 
     <ul class="text-gray-300">
       <li class="mb-4">
         <a href="#faq" class="hover:underline">FAQ
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/ngo" class="hover:underline"> Dla NGO
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/business" class="hover:underline">Dla biznesu
+        </a>
+      </li>
+      <li class="mb-4">
+        <a href="/volunteer" class="hover:underline">Dla IT wolontariutu
+        </a>
+      </li>
+      <!--<FooterLink>--> 
+      <li class="mb-4">
+        <a href="/startups" class="hover:underline">Dla startupow
         </a>
       </li>
       <!--<FooterLink>--> 
@@ -22,11 +132,19 @@
         <a href="#contact" class="hover:underline">Już pomogliśmy 
         </a>
       </li>
+      <li class="mb-4">
+        <a href="#contact" class="hover:underline">Recenzje o nas
+        </a>
+      </li>
+      <!--<FooterLink>--> 
+
+      <!--<FooterLink>--> 
+
       <!--<FooterLink>--> 
     </ul>
     <!--<FooterLinkGroup>-->
-  </div> 
-  <div>
+  </div>
+    <div>
     <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Kontakt
     </h2> 
     <ul class="text-gray-300">
@@ -54,55 +172,6 @@
     </ul>
     <!--<FooterLinkGroup>-->
   </div> 
-  <div>
-    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Legal
-    </h2> 
-    <ul class="text-gray-300">
-      <li class="mb-4">
-        <a href="/" class="hover:underline">Privacy Policy
-        </a>
-      </li>
-      <!--<FooterLink>--> 
-      <li class="mb-4">
-        <a href="/" class="hover:underline">Licensing
-        </a>
-      </li>
-      <!--<FooterLink>--> 
-      <li class="mb-4">
-        <a href="/" class="hover:underline">Terms &amp; Conditions
-        </a>
-      </li>
-      <!--<FooterLink>-->
-    </ul>
-    <!--<FooterLinkGroup>-->
-  </div> 
-  <div>
-    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Wspieramy wsperajiencych
-    </h2> 
-    <ul class="text-gray-300">
-      <li class="mb-4">
-        <a href="/ngo" class="hover:underline">Dla NGO
-        </a>
-      </li>
-      <!--<FooterLink>--> 
-      <li class="mb-4">
-        <a href="/startups" class="hover:underline">Dla startupow
-        </a>
-      </li>
-      <!--<FooterLink>--> 
-      <li class="mb-4">
-        <a href="/business" class="hover:underline">Dla biznesu
-        </a>
-      </li>
-      <!--<FooterLink>--> 
-      <li class="mb-4">
-        <a href="/volounteers" class="hover:underline">Dla IT wolontariutu
-        </a>
-      </li>
-      <!--<FooterLink>--> 
-    </ul>
-    <!--<FooterLinkGroup>-->
-  </div>
 </div> 
 <div class="py-6 px-4 bg-violDark md:flex md:items-center md:justify-between">
   <span class="text-sm text-gray-300 sm:text-center">© 2022 
