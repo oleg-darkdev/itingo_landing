2cac3019a910faf45e8addc304214d1db043ffaf
DarkDev <darkdev_brand@riseup.net>
2022-10-25 14:12:27 +0300

    upd text

--- src/layout/landing/WeDoForBlock.svelte
+++ src/layout/landing/WeDoForBlock.svelte
@@ -3,17 +3,20 @@
 </script>
 
 <div class="flex w-full justify-center relative ">
-  <div style="min-height: 320px; margin-top: -180px;" class="text-center z-2 flex max-w-2xl flex-col bg-viol shadow-md text-gray-500 dark:text-gray-400 rounded-lg  p-4 text-center">
+  <div style="min-height: 260px; margin-top: -180px;" class="text-center z-2 flex max-w-2xl flex-col bg-viol shadow-md text-gray-500 dark:text-gray-400 rounded-lg  p-4 text-center">
     <h5 class="mb-2 text-3xl font-bold text-white">Wspieramy wsperajiencych</h5>
     <p class="mb-5 text-base text-white sm:text-lg dark:text-gray-400">
-      Vollie is an online marketplace that connects skilled people to non-profits, charities and social enterprises for skills-based online volunteering. Vollie projects are exclusively online, meaning that people can donate their skills and experience from anywhere in the world, and around their busy personal and professional schedule. Sign up today and start changing the world!
+		  ITingo to platforma internetowa, która łączy IT , wolontariuszy, organizacje pozarządowe, biznes i start-upy - w celu dostarczenia projektów IT online.
+    </p>
+    <p class="mb-3 text-center  text-base text-white sm:text-lg  dark:text-gray-400 leading-tight">
+      Zarejestruj się i zacznij zmieniać świat organizacji pozarządowych!
     </p>
     <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
       <ButtonGroup>
-          <Button outline size="lg" color="yellow">Dla NGO</Button>
-          <Button outline size="lg" color="yellow">Dla startupow</Button>
-          <Button outline size="lg" color="yellow">Dla biznesu</Button>
-          <Button outline size="lg" color="yellow">Dla IT wolontariutu</Button>
+          <Button outline size="lg" color="yellow">Jako NGO</Button>
+          <Button outline size="lg" color="yellow">Jako startup</Button>
+          <Button outline size="lg" color="yellow">Jako biznes</Button>
+          <Button outline size="lg" color="yellow">Jako IT wolontariat</Button>
       </ButtonGroup>
     </div>
   </div>
