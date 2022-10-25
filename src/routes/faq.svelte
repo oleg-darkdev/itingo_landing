557a8004d62360811a5fc7b4e3d5a5aad95cd701
DarkDev <darkdev_brand@riseup.net>
2022-10-25 16:57:50 +0300

    alfa v.

---
+++ src/routes/faq.svelte
@@ -0,0 +1,23 @@
+<script>
+	import FaqSection from '../layout/FaqSection.svelte';
+	import mainfaqList from '../data/landing/landingFaqList';
+	import ngoFaqList from '../data/ngo/ngoFaqList';
+	import businessFaqList from '../data/ngo/ngoFaqList';
+	import volounteersFaqList from '../data/ngo/ngoFaqList';
+	
+</script>
+
+<svelte:head>
+	<title>ITingo - FAQ</title>
+</svelte:head>
+
+
+<FaqSection faqList={mainfaqList}/>
+<FaqSection faqList={ngoFaqList}/>
+<FaqSection faqList={businessFaqList}/>
+<FaqSection faqList={volounteersFaqList}/>
+
+
+<style>
+    
+</style>
