dd358bb65556d6e00f9d86601f2afb8bcab42cdd
DarkDev <darkdev_brand@riseup.net>
2022-10-28 02:56:27 +0300

    proto v.

---
+++ src/data/ngo/partnersList.js
@@ -0,0 +1,11 @@
+let typesWork = [
+	{
+		bgImage: '/images/ngo/bg/design-bg.svg',
+		title: 'Design',
+		description: ['', ''],
+		examples: ['', '', '', ''],
+		align: 'start',
+	},
+];
+
+export default typesWork;
--- src/data/ngo/workTypes.js
+++ src/data/ngo/workTypes.js
@@ -5,56 +5,64 @@ let typesWork = [
 		description: ['', ''],
 		examples: ['', '', '', ''],
 		align: 'start',
-		anchor: 'design'
+		anchor: 'design',
+		dataAboutService: {
+			title: 'Dołącz',
+			subTitle: '',
+			description: [
+				'Pomożemy organizacjom pozarządowym urzeczywistnić projekty stron internetowych. Przez specjalny formularz opowiadasz nam, co chcesz osiągnąć, a my pomożemy sformułować projekt, który przyciągnie odpowiednich wolontariuszy.',
+				'Jesteśmy po to, aby ułatwić życie trzeciego sektora!'
+			]
+		}
 	},
-	{
-		bgImage: '/images/ngo/bg/projecting-bg.svg',
-		title: 'Projecting',
-		description: ['', ''],
-		examples: ['', '', '', ''],
-		align: 'end',
-		anchor: 'projecting'
-	},
-	{
-		bgImage: '/images/ngo/bg/security-bg.svg',
-		title: 'Security',
-		description: ['', ''],
-		examples: ['', '', '', ''],
-		align: 'start',
-		anchor: 'security'
-	},
-	{
-		bgImage: '/images/ngo/bg/seo-bg.svg',
-		title: 'Seo',
-		description: ['', ''],
-		examples: ['', '', '', ''],
-		align: 'end',
-		anchor: 'seo'
-	},
-	{
-		bgImage: '/images/ngo/bg/server-bg.svg',
-		title: 'Server',
-		description: ['', ''],
-		examples: ['', '', '', ''],
-		align: 'start',
-		anchor: 'server'
-	},
-	{
-		bgImage: '/images/ngo/bg/site-bg.svg',
-		title: 'Site',
-		description: ['', ''],
-		examples: ['', '', '', ''],
-		align: 'end',
-		anchor: 'site'
-	},
-	{
-		bgImage: '/images/ngo/bg/social-bg.svg',
-		title: 'Social Media',
-		description: ['', ''],
-		examples: ['', '', '', ''],
-		align: 'start',
-		anchor: 'media'
-	}
+	// {
+	// 	bgImage: '/images/ngo/bg/projecting-bg.svg',
+	// 	title: 'Projecting',
+	// 	description: ['', ''],
+	// 	examples: ['', '', '', ''],
+	// 	align: 'end',
+	// 	anchor: 'projecting'
+	// },
+	// {
+	// 	bgImage: '/images/ngo/bg/security-bg.svg',
+	// 	title: 'Security',
+	// 	description: ['', ''],
+	// 	examples: ['', '', '', ''],
+	// 	align: 'start',
+	// 	anchor: 'security'
+	// },
+	// {
+	// 	bgImage: '/images/ngo/bg/seo-bg.svg',
+	// 	title: 'Seo',
+	// 	description: ['', ''],
+	// 	examples: ['', '', '', ''],
+	// 	align: 'end',
+	// 	anchor: 'seo'
+	// },
+	// {
+	// 	bgImage: '/images/ngo/bg/server-bg.svg',
+	// 	title: 'Server',
+	// 	description: ['', ''],
+	// 	examples: ['', '', '', ''],
+	// 	align: 'start',
+	// 	anchor: 'server'
+	// },
+	// {
+	// 	bgImage: '/images/ngo/bg/site-bg.svg',
+	// 	title: 'Site',
+	// 	description: ['', ''],
+	// 	examples: ['', '', '', ''],
+	// 	align: 'end',
+	// 	anchor: 'site'
+	// },
+	// {
+	// 	bgImage: '/images/ngo/bg/social-bg.svg',
+	// 	title: 'Social Media',
+	// 	description: ['', ''],
+	// 	examples: ['', '', '', ''],
+	// 	align: 'start',
+	// 	anchor: 'media'
+	// }
 ];
 
 export default typesWork;
