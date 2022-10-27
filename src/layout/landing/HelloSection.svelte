65c27657ceb331528f93eee4ca1193024c524a58
DarkDev <darkdev_brand@riseup.net>
2022-10-28 02:50:41 +0300

    fix the description text

--- src/layout/landing/HelloSection.svelte
+++ src/layout/landing/HelloSection.svelte
@@ -7,7 +7,7 @@
 	<Card style="" class="mb-32" img="/images/landing/hello.svg" horizontal reverse={false}>
     <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-violDark dark:text-white">Jak <span class="mb-2 text-3xl font-black" >ITingo</span> ulatwia IT wolontariat</h5>
     <p class="mb-3 text-center font-normal text-viol dark:text-gray-400 leading-tight">
-		ITingo to platforma internetowa, która łączy IT , wolontariuszy, organizacje pozarządowe, biznes i start-upy - w celu dostarczenia projektów IT online.
+		ITingo to platforma internetowa, która łączy IT wolontariat, organizacje pozarządowe, biznes i start-upy - w celu dostarczenia projektów IT online.
     </p>
     <p class="mb-3 text-center font-normal text-viol dark:text-gray-400 leading-tight">
 		Zarejestruj się już dziś i zacznij zmieniać świat organizacji pozarządowych!
