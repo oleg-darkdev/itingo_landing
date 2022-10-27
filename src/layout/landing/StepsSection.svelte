5b54147874cf7f527a2a1f33b7bd5e8e78d40983
DarkDev <darkdev_brand@riseup.net>
2022-10-28 02:54:47 +0300

    fix to mobile

--- src/layout/landing/StepsSection.svelte
+++ src/layout/landing/StepsSection.svelte
@@ -1,12 +1,12 @@
 <script>
-    import Timeline from '../../components/landing/Timeline.svelte';
+    import Timeline from '../../components/landing/Steps.svelte';
 	import Registration from './RegistrationBlock.svelte';
 </script>
 
 
 <section class="pt-12 w-full flex flex-col items-center" id="steps">
-	<div class="lg:w-8/12 md:w-10/12  justify-center flex-col p-0"> 
-		<h2 class="w-full mt-8 mb-8 text-center text-3xl sm:text-lg text-viol font-bold ">Znajdz wykwalifikowanych IT wolontariuszy w 3 prostych krokach</h2>
+	<div class="lg:w-8/12 md:w-10/12 w-12/12 justify-center flex-col p-0"> 
+		<h2 class="w-full mt-8 mb-8 text-center lg:text-6xl md:text-6xl text-3xl  text-viol font-bold ">Znajdz wykwalifikowanych IT wolontariuszy w 3 prostych krokach</h2>
 		<Timeline />
 		
 	</div>
