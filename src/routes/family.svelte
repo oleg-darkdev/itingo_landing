8401c7accfb8cdb1a1f7c3579922aa15d0e14986
DarkDev <darkdev_brand@riseup.net>
2022-10-28 02:57:24 +0300

    proto v.

---
+++ src/routes/family.svelte
@@ -0,0 +1,37 @@
+<script>
+	import FaqSection from '../layout/FaqSection.svelte';
+	import ContactSection from '../layout/landing/ContactSection.svelte';
+
+	import OurFamily from '../layout/ngo/OurFamily.svelte';
+	import partnersList from '../data/ngo/partnersList';
+	import faqList from '../data/ngo/ngoFaqList';
+
+	
+</script>
+
+<svelte:head>
+	<title>ITingo dla NGO</title>
+</svelte:head>
+
+
+
+<!-- 
+    1. sponsors
+    2. bussines partnewrs
+    3. ngp partners
+    4. volonteers
+    5. startups
+
+
+
+ -->
+{#each partnersList as member}
+	<OurFamily {member} anchor='partners'/>
+{/each}
+
+
+
+<a id="faq" ></a>
+<FaqSection {faqList}/>
+
+<ContactSection />
