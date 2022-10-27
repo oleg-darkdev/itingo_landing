3cfa882079a0142c77842ffddf73f496b54c75fc
DarkDev <darkdev_brand@riseup.net>
2022-10-28 02:54:21 +0300

    fix to mobile

--- src/layout/landing/WeDoForBlock.svelte
+++ src/layout/landing/WeDoForBlock.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div class="flex w-full justify-center relative ">
-  <div style="min-height: 260px; margin-top: -180px;" class="sm:max-w-max text-center z-2 flex w-5/12 flex-col bg-viol shadow-md text-gray-500 dark:text-gray-400 rounded-lg  lg:p-4 sm:p-1 text-center">
+  <div style="min-height: 260px; height: auto; margin-top: -180px;" class="sm:max-w-max md:w-8/12 text-center z-2 flex lg:w-5/12 flex-col bg-viol shadow-md text-gray-500 dark:text-gray-400 rounded-lg  lg:p-4 p-2 text-center">
     <h5 class="mb-2 text-3xl font-bold text-white">Wspieramy wsperajiencych</h5>
     <p class="mb-5 text-base text-white sm:text-lg dark:text-gray-400">
-		  ITingo to platforma internetowa, która łączy IT , wolontariuszy, organizacje pozarządowe, biznes i start-upy - w celu dostarczenia projektów IT online.
+		  <span class="text-2xl text-lightYellow font-black">ITingo</span> to platforma internetowa, która łączy IT , wolontariuszy, organizacje pozarządowe, biznes i start-upy - w celu dostarczenia projektów IT online.
     </p>
     <p class="mb-3 text-center  text-base text-white sm:text-lg  dark:text-gray-400 leading-tight">
       Zarejestruj się i zacznij zmieniać świat organizacji pozarządowych!
