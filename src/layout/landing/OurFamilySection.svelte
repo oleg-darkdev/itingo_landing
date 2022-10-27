83818aad3c2c9e51d5557e49b4b977b7214c0ad2
DarkDev <darkdev_brand@riseup.net>
2022-10-28 02:55:13 +0300

    add link

--- src/layout/landing/OurFamilySection.svelte
+++ src/layout/landing/OurFamilySection.svelte
@@ -7,7 +7,7 @@
 	<div style="" class="text-wrap lg:w-5/12 md:w-12/12 sm:w-12/12  flex justify-end items-center">
 		<div style="min-width: 320px;" class="pt-6 pb-12 text-block max-w-max flex flex-col items-center rounded-xl">
 			<h2 class=" m-8  text-6xl text-lightYellow font-bold ">NASZA <br> NON-PROFIT <br> RODZINA</h2>
-			<Button size="xl" class="w-10/12" color="purple">Czytać więcej</Button>
+			<Button href="/family" size="xl" class="w-10/12" color="purple">Czytać więcej</Button>
 		</div>
 	</div>
 </section>
