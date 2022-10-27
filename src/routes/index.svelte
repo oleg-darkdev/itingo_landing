0d39887bfe0bbcfad25ce18c0cafee8236100d53
DarkDev <darkdev_brand@riseup.net>
2022-10-28 02:54:00 +0300

    fix text, fix responsive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,7 @@
 	import OurFamilySection from '../layout/landing/OurFamilySection.svelte';
 	import FaqSection from '../layout/FaqSection.svelte';
 	import ContactSection from '../layout/landing/ContactSection.svelte';
-	import SocialMedia from "../layout/SocialMedia.svelte";
+	import SocialMedia from "../layout/contact/SocialMedia.svelte";
 	import faqList from "../data/landing/landingFaqList";
 </script>
 
@@ -18,8 +18,10 @@
 
 
 <section class="banner items-center  w-full  flex flex-col justify-center">
-	<div style="" class="h-auto p-2 bg-viol lg:w-5/12 md:w-6/12  sm:w-10/12  flex flex-col rounded-xl justify-center">
-		<h1 class="w-full text-center lg:text-7xl md:text-6xl text-5xl text-white font-black mb-8"><span class="text-lightYellow">ITingo</span> - dom IT wolontariatu</h1>
+	<div style="" class="h-auto p-2 bg-viol lg:w-5/12 md:w-6/12  w-12/12  flex flex-col rounded-xl justify-center">
+		<h1 class="w-full text-center lg:text-7xl md:text-6xl text-5xl text-white font-black mb-8">
+			<span class="text-lightYellow text-stantley">ITingo</span>
+			 - dom IT wolontariatu</h1>
 		<SocialMedia />
 	</div>
 
@@ -63,7 +65,14 @@
 		background-size: auto;
 		background-attachment: fixed;
 	}
-
+	@media (min-device-width: 320px) and (max-device-width: 1024px) {
+		.banner {
+			background-size: 100%; 
+			margin-bottom: 10px;
+			background-size: cover;
+			background-position: right;
+		}
+	}
 
 </style>
 
