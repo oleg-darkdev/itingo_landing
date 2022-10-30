b8b7fe9c4536e6e8223d5898a701df6a02a5c262
DarkDev <darkdev_brand@riseup.net>
2022-10-31 01:31:30 +0200

    fix view

--- src/layout/ngo/TypesOfWorks.svelte
+++ src/layout/ngo/TypesOfWorks.svelte
@@ -8,18 +8,18 @@
 <!-- class:active={}  -->
 <section id={typeWork.anchor} style="background-position: {typeWork.align == 'start' ? 'right': 'left'}; background-image: url({typeWork.bgImage});" class="our-family justify-{typeWork.align} content-center pr-20" >
 	<div style="" class="lg:w-7/12 md:w-12/12 sm:w-12/12 bg-white flex justify-end items-center">
-		<div style="min-width: 320px;" class="flex flex-col items-start">
-			<h2 class=" m-8  text-6xl text-lightYellow font-bold ">{typeWork.title}</h2>
-
+		<div style="min-width: 320px;" class="flex flex-col items-center">
+			<h2 class=" m-8  text-6xl text-lightYellow font-bold">{typeWork.title}</h2>
+			  <p class="font-normal w-8/12 text-start leading-tight mb-2 text-viol">
+  				{typeWork.description}
+				</p>
 			<WorkList dataAboutService={typeWork.dataAboutService}/>
-
-
-
-			<Button href='/form' size="xl" class="w-10/12" color="purple">Zgłosić projekt</Button>
+			<Button href='/form' size="xl" class="lg:w-4/12" color="purple">Zgłosić projekt</Button>
 		</div>
 	</div>
 </section>
 
+			 
 
 <style>
     .our-family {
