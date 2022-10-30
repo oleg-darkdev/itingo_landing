13e8fe4430fb7ed8eaf1d9c5e9abab653359e65a
DarkDev <darkdev_brand@riseup.net>
2022-10-31 01:29:48 +0200

    upd text

--- src/data/ngo/workTypes.js
+++ src/data/ngo/workTypes.js
@@ -1,68 +1,233 @@
+
+// 				consulting: {
+// 					tools: false,
+// 					site: false,
+// 					other: false
+// 				},
+
 let typesWork = [
 	{
 		bgImage: '/images/ngo/bg/design-bg.svg',
 		title: 'Design',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+		// examples: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
+		align: 'start',
+		anchor: 'design',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Social Media',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Poster',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Visite card',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Email template',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/ngo/bg/projecting-bg.svg',
+		title: 'Projecting',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+		// examples: ['', '', '', ''],
+		align: 'end',
+		anchor: 'projecting',
+		dataAboutService: [
+			{
+				title: 'Ux UI',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Brand book',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/ngo/bg/security-bg.svg',
+		title: 'Security',
 		description: ['', ''],
 		examples: ['', '', '', ''],
 		align: 'start',
-		anchor: 'design',
-		dataAboutService: {
-			title: 'Dołącz',
-			subTitle: '',
-			description: [
-				'Pomożemy organizacjom pozarządowym urzeczywistnić projekty stron internetowych. Przez specjalny formularz opowiadasz nam, co chcesz osiągnąć, a my pomożemy sformułować projekt, który przyciągnie odpowiednich wolontariuszy.',
-				'Jesteśmy po to, aby ułatwić życie trzeciego sektora!'
-			]
-		}
+		anchor: 'security',
+		dataAboutService: [
+			{
+				title: 'Analyse & Consulting',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Hacking',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
 	},
-	// {
-	// 	bgImage: '/images/ngo/bg/projecting-bg.svg',
-	// 	title: 'Projecting',
-	// 	description: ['', ''],
-	// 	examples: ['', '', '', ''],
-	// 	align: 'end',
-	// 	anchor: 'projecting'
-	// },
-	// {
-	// 	bgImage: '/images/ngo/bg/security-bg.svg',
-	// 	title: 'Security',
-	// 	description: ['', ''],
-	// 	examples: ['', '', '', ''],
-	// 	align: 'start',
-	// 	anchor: 'security'
-	// },
-	// {
-	// 	bgImage: '/images/ngo/bg/seo-bg.svg',
-	// 	title: 'Seo',
-	// 	description: ['', ''],
-	// 	examples: ['', '', '', ''],
-	// 	align: 'end',
-	// 	anchor: 'seo'
-	// },
-	// {
-	// 	bgImage: '/images/ngo/bg/server-bg.svg',
-	// 	title: 'Server',
-	// 	description: ['', ''],
-	// 	examples: ['', '', '', ''],
-	// 	align: 'start',
-	// 	anchor: 'server'
-	// },
-	// {
-	// 	bgImage: '/images/ngo/bg/site-bg.svg',
-	// 	title: 'Site',
-	// 	description: ['', ''],
-	// 	examples: ['', '', '', ''],
-	// 	align: 'end',
-	// 	anchor: 'site'
-	// },
-	// {
-	// 	bgImage: '/images/ngo/bg/social-bg.svg',
-	// 	title: 'Social Media',
-	// 	description: ['', ''],
-	// 	examples: ['', '', '', ''],
-	// 	align: 'start',
-	// 	anchor: 'media'
-	// }
+	{
+		bgImage: '/images/ngo/bg/seo-bg.svg',
+		title: 'Seo',
+		description: ['', ''],
+		examples: ['', '', '', ''],
+		align: 'end',
+		anchor: 'seo',
+		dataAboutService: [
+			{
+				title: 'Write text',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Analyse & Consulting',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/ngo/bg/server-bg.svg',
+		title: 'Server',
+		description: ['', ''],
+		examples: ['', '', '', ''],
+		align: 'start',
+		anchor: 'server',
+		dataAboutService: [
+			{
+				title: 'Data base',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Server configuration',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Analyse & Consulting',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/ngo/bg/site-bg.svg',
+		title: 'Website',
+		description: ['', ''],
+		examples: ['', '', '', ''],
+		align: 'end',
+		anchor: 'site',
+		dataAboutService: [
+			{
+				title: 'Create',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Update',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Buy domain',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Others things',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/ngo/bg/social-bg.svg',
+		title: 'Social Media',
+		description: ['', ''],
+		examples: ['', '', '', ''],
+		align: 'start',
+		anchor: 'media',
+		dataAboutService: [
+			{
+				title: 'Strategy',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Consulting',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			},
+			{
+				title: 'Design',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	}
 ];
 
 export default typesWork;
