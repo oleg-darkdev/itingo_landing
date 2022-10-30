8de8cc48910fbc60e2b914c8e7e007aff8f35759
DarkDev <darkdev_brand@riseup.net>
2022-10-31 01:27:09 +0200

    alpha v.

---
+++ src/routes/workspace.svelte
@@ -0,0 +1,31 @@
+<script>
+  import WorkspaceSection from "../layout/WorkspaceSection.svelte";
+  
+  import VolunteerСard from '../components/workspace/VolunteerСard.svelte';
+  import volunteerList from '../data/volunteersList';
+
+  let sectionDataFrontEnd = {
+    title: 'Front end',
+    howMuch: volunteerList.frontEnd.length,
+    topTitle: 'Top Volunteers',
+    subTitle: 'Number of volunteers available',
+    link: '/volunteers/front-end'
+  };
+
+
+  let topVolsFront = volunteerList.frontEnd.slice(0,2);
+</script>
+
+
+<WorkspaceSection sectionData={sectionDataFrontEnd}>
+  <div slot="top" class="flex flex-row flex-wrap justify-center ">
+    {#each topVolsFront as volunteerData}
+      <VolunteerСard {volunteerData} />
+    {/each}
+  </div>
+  <div slot="all" class="projects-wrap w-full rounded-xl  flex flex-row flex-wrap justify-center mb-16">
+    {#each volunteerList.frontEnd.slice(0,6) as volunteerData}
+      <VolunteerСard {volunteerData} />
+    {/each}
+  </div>
+</WorkspaceSection>
