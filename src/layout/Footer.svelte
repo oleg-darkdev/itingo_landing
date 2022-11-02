5e1c1a78c1c9a8a56626417b1b3cb073cafff4ab
DarkDev <darkdev_brand@riseup.net>
2022-11-02 16:57:17 +0200

    upd links to vol`s page, clear

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -12,44 +12,34 @@
         <a href="/volunteer#start" class="hover:underline">Zostań IT wolontariuszem
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
       <li class="mb-4">
         <a href="/volunteer#faq " class="hover:underline">FAQ
         </a>
       </li>
       <li class="mb-4">
-        <a href="/volunteer#design" class="hover:underline">Pomóc jako grafica
+        <a href="/volunteer#work" class="hover:underline">Rodzaje prac
         </a>
       </li>
       <li class="mb-4">
-        <a href="/volunteer#projecting" class="hover:underline">Pomóc z projektowaniem stron
+        <a href="/volunteer#mentoring" class="hover:underline">Pomoc dla początkujących w IT & Mentoring 
         </a>
       </li>
       <li class="mb-4">
-        <a href="/volunteer#security" class="hover:underline">Pomóc z bezpieczeństwom IT
+        <a href="/volunteer#letter" class="hover:underline">Pomoc w zatrudnieniu
         </a>
       </li>
       <li class="mb-4">
-        <a href="/volunteer#seo" class="hover:underline">Pomóc z Seo
+        <a href="/volunteer#schedule" class="hover:underline">Wolny harmonogram udziału
         </a>
       </li>
       <li class="mb-4">
-        <a href="/volunteer#server" class="hover:underline">Pomóc na serwerze
+        <a href="/volunteer#technology" class="hover:underline">Swoboda wyboru technologii dla projektów
         </a>
       </li>
-      <li class="mb-4">
-        <a href="/volunteer#site" class="hover:underline">Pomóc z tworzeniem strony
-        </a>
-      </li>
-      <li class="mb-4">
-        <a href="/volunteer#social-media" class="hover:underline">Pomoc w zakresie mediów 
-        </a>
-      </li>
-      <!--<FooterLink>--> 
-
-      <!--<FooterLink>-->
+      
     </ul>
-    <!--<FooterLinkGroup>-->
+    
   </div> 
   <div>
     <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Wspieramy wsperajiencych
@@ -71,17 +61,17 @@
         <a href="/volunteer" class="hover:underline">Dla IT wolontariutu
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
       <li class="mb-4">
         <a href="/startups" class="hover:underline">Dla startupow
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
       <li class="mb-4">
         <a href="/family" class="hover:underline">Nasza rodzina
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
       <li class="mb-4">
         <a href="#contact" class="hover:underline">Już pomogliśmy 
         </a>
@@ -90,13 +80,13 @@
         <a href="#contact" class="hover:underline">Recenzje o nas
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
 
-      <!--<FooterLink>--> 
+      
 
-      <!--<FooterLink>--> 
+      
     </ul>
-    <!--<FooterLinkGroup>-->
+    
   </div>
    <div>
     <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Dla NGO
@@ -106,7 +96,7 @@
         <a href="/ngo#volunteer" class="hover:underline">Znajdź IT wolontariusza
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
       <li class="mb-4">
         <a href="/ngo#faq " class="hover:underline">FAQ
         </a>
@@ -139,11 +129,11 @@
         <a href="/ngo#social-media" class="hover:underline">Pomoc w zakresie mediów 
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
 
-      <!--<FooterLink>-->
+      
     </ul>
-    <!--<FooterLinkGroup>-->
+    
   </div> 
     <div>
     <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Kontakt
@@ -153,25 +143,25 @@
         <a href="/" class="hover:underline">Twitter
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
       <li class="mb-4">
         <a href="/" class="hover:underline">Facebook
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
       <li class="mb-4">
         <a href="/" class="hover:underline">Instagram
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
         
       <li class="mb-4">
         <a href="https://github.com/oleg-darkdev/itingo" class="hover:underline">Github
         </a>
       </li>
-      <!--<FooterLink>--> 
+      
     </ul>
-    <!--<FooterLinkGroup>-->
+    
   </div> 
 </div> 
 <div class="py-6 px-4 bg-violDark md:flex md:items-center md:justify-between">
