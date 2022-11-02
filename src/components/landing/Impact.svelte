c231e9314e7bf26ce1d777011a3bc530c52e526a
DarkDev <darkdev_brand@riseup.net>
2022-11-02 17:00:50 +0200

    refactoring, add animation for numbers

--- src/components/landing/Impact.svelte
+++ src/components/landing/Impact.svelte
@@ -1,43 +1,42 @@
 <script>
-  import { Card } from "flowbite-svelte";
+    import { Card } from "flowbite-svelte";
+    import { CountUp } from 'countup.js';
+	import { onMount } from 'svelte';
 
-  const impactData = [
-    {
-        reverse: false,
-        numbers: 2,
-        title: 'Realizowane projekty',
-        img: '/images/landing/impact/projects_impact.svg'
-    },
-    {
-        reverse: true,
-        numbers: 1,
-        title: 'Zatrudnieni wolontariusze',
-        img: '/images/landing/impact/volunteers_impact.svg'
-    },
-    {
-        reverse: false,
-        numbers: 200,
-        title: 'Godzin wolontariatu',
-        img: '/images/landing/impact/hours_impact.svg'
-    },
-    {
-        reverse: true,
-        numbers: 2,
-        title: 'Zakładane startupow',
-        img: '/images/landing/impact/startups_impact.svg'
-    },
-  ]
+	onMount(() => {
+    const progectsCountUp = new CountUp('progects', 4, { enableScrollSpy: true, duration: 20 }),
+        volunteersCountUp = new CountUp('volunteers', 36, { enableScrollSpy: true, duration: 12 }),
+        hoursCountUp = new CountUp('hours', 200, { enableScrollSpy: true, duration: 15 }),
+        startupsCountUp = new CountUp('startups', 2, { enableScrollSpy: true, duration: 15 });
+	});
+    
 </script>
 
 
 
 <div style="justify-content: space-around;" class="flex flex-wrap row lg:w-10/12 md:w-12/12 sm:w-12/12 ">
-    {#each impactData as impact}
-    <Card  class="m-2" img={impact.img} horizontal reverse={impact.reverse}>
-        <h5 class="mb-2 text-8xl font-bold tracking-tight text-viol dark:text-white">{impact.numbers}</h5>
+    <Card  class="m-2" img='/images/landing/impact/projects_impact.svg' horizontal reverse='false'>
+        <h5 id="progects" class="mb-2 text-8xl font-bold tracking-tight text-viol dark:text-white"></h5>
         <p style="width: 250px;" class="mb-3 text-2xl font-normal text-violDark dark:text-gray-400 leading-tight">
-        {impact.title}
+            Realizowane projekty
         </p>
     </Card>
-    {/each}
+    <Card  class="m-2" img='/images/landing/impact/volunteers_impact.svg' horizontal reverse='true'>
+        <h5 id="volunteers" class="mb-2 text-8xl font-bold tracking-tight text-viol dark:text-white"></h5>
+        <p style="width: 250px;" class="mb-3 text-2xl font-normal text-violDark dark:text-gray-400 leading-tight">
+        Zatrudnieni wolontariusze
+        </p>
+    </Card>
+    <Card  class="m-2" img='/images/landing/impact/hours_impact.svg' horizontal reverse='false'>
+        <h5 id="hours" class="mb-2 text-8xl font-bold tracking-tight text-viol dark:text-white"></h5>
+        <p style="width: 250px;" class="mb-3 text-2xl font-normal text-violDark dark:text-gray-400 leading-tight">
+        Godzin wolontariatu
+        </p>
+    </Card>
+    <Card  class="m-2" img='/images/landing/impact/startups_impact.svg' horizontal reverse='true'>
+        <h5 id="startups" class="mb-2 text-8xl font-bold tracking-tight text-viol dark:text-white"></h5>
+        <p style="width: 250px;" class="mb-3 text-2xl font-normal text-violDark dark:text-gray-400 leading-tight">
+        Zakładane startupow
+        </p>
+    </Card>
 </div>
