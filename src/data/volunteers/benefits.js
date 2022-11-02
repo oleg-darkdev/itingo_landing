57997f5793e8650c36ebf386b1cf37c21fa98f16
DarkDev <darkdev_brand@riseup.net>
2022-11-02 16:56:44 +0200

    alfa v. the list of benefits for vol`s

---
+++ src/data/volunteers/benefits.js
@@ -0,0 +1,120 @@
+let benefits = [
+	{
+		bgImage: '/images/volunteers/benefits/reccomandation_letter-bg.svg',
+		title: 'Dowód doświadczenia',
+		description: ['Dowód doświadczenia w programowaniu oraz listy polecające do zatrudnienia.'],
+		align: 'start',
+		anchor: 'letter',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/volunteers/benefits/mentoring-bg.svg',
+		title: 'Mentoring',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+
+		align: 'end',
+		anchor: 'mentoring-',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/volunteers/benefits/flexible_schedule-bg.svg',
+		title: 'Wolny harmonogram udziału w projektach',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+
+		align: 'start',
+		anchor: 'schedule',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/volunteers/benefits/begginers_help-bg.svg',
+		title: 'Pomoc dla początkujących',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+
+		align: 'end',
+		anchor: 'design',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/volunteers/benefits/progress-bg.svg',
+		title: 'Nieustanny rozwój',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+
+		align: 'start',
+		anchor: 'progress',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: '/images/volunteers/benefits/technology-bg.svg',
+		title: 'Swoboda wyboru technologii dla projektów',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+
+		align: 'end',
+		anchor: 'technology',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	}
+];
+
+export default benefits;
