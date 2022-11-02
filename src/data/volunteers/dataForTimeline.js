42784a60564003772063f503281849fcc627d59c
DarkDev <darkdev_brand@riseup.net>
2022-11-02 16:59:08 +0200

    proto v. timeline

---
+++ src/data/volunteers/dataForTimeline.js
@@ -0,0 +1,31 @@
+const dataSteps = [
+	{
+		step: 1,
+		title: 'Dołącz',
+		description: [
+			'Pomożemy organizacjom pozarządowym urzeczywistnić projekty stron internetowych. Przez specjalny formularz opowiadasz nam, co chcesz osiągnąć, a my pomożemy sformułować projekt, który przyciągnie odpowiednich wolontariuszy.',
+			'Jesteśmy po to, aby ułatwić życie trzeciego sektora!'
+		]
+	},
+	{
+		step: 2,
+		title: 'Publikacja',
+		description: [
+			'Wolontariusze odbierają projekty, rozpoczynają projekt od podstaw  lub które zostały już zrealizowane i wprowadzają niezbędne zmiany, co umożliwia rozwój dobrych czasów (i organizacji pozarządowych)! ',
+			'Kandydaci są przesłuchiwani w razie potrzeby, aby wybrać odpowiedniego wolontariusza do projektu.',
+			'Uczestnicy projektu wybierają plan projektu i zakres zadań do realizacji, a następnie przystępują do pracy.'
+		]
+	},
+	{
+		step: 3,
+		title: 'Współpraca',
+		description: [
+			'Komunikuj się poprzez platformę ITingo z biznesem, wykwalifikowanymi wolontariuszami, przesyłaj opinie i komentarze -w końcu trafisz na projekt, który Ci się spodoba.',
+			'I w końcu wykreślimy razem ten kłopotliwy drobiazg `IT` - ze swojej listy problemów!'
+		]
+	}
+];
+
+
+
+export default dataSteps;
