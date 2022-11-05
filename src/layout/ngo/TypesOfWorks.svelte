a315230e9ef14d3fc15c4030302688faaf8fc0cd
DarkDev <darkdev_brand@riseup.net>
2022-11-05 02:12:07 +0200

    fix for mobile

--- src/layout/ngo/TypesOfWorks.svelte
+++ src/layout/ngo/TypesOfWorks.svelte
@@ -6,16 +6,15 @@
 </script>
 
 <!-- class:active={}  -->
-<section id={typeWork.anchor} style="background-position: {typeWork.align == 'start' ? 'right': 'left'}; background-image: url({typeWork.bgImage});" class="our-family justify-{typeWork.align} content-center lg:pr-20 md:pr-20" >
+<section id={typeWork.anchor} style="background-position: {typeWork.align == 'start' ? 'right': 'left'}; background-image: url({typeWork.bgImage});" class="work-wrap justify-{typeWork.align} content-center " >
 	<div data-aos="fade-{typeWork.align == 'start' ? 'right': 'left'}"
 		data-aos-delay="80"
 		data-aos-duration="800" 
 		data-aos-once="false"
 		data-aos-mirror="true"  style="" class="lg:w-7/12 md:w-12/12 bg-white flex justify-end items-center">
 		<div 
-		
-		style="min-width: 320px;" class="flex flex-col items-center ">
-			<h2 class=" m-8  text-6xl text-lightYellow font-bold">{typeWork.title}</h2>
+		style="min-width: 320px;" class="pt-8 pb-8 flex flex-col items-center ">
+			<h2 class=" m-8 text-6xl text-lightYellow font-bold">{typeWork.title}</h2>
 			  <p class="font-normal lg:w-8/12 md:w-8/12 w-11/12 text-start leading-tight mb-2 text-viol">
   				{typeWork.description}
 			</p>
@@ -27,11 +26,22 @@
 
 
 <style>
-    .our-family {
-		min-height: 100vh;
+    .work-wrap {
+		
 		height: auto;
 		background-repeat: no-repeat;		
 		background-size: auto;
 		background-attachment: fixed;
+	}
+	@media (min-width: 1024px) {
+		.work-wrap {
+			min-height: 100vh;
+		}
+	}
+	@media (min-device-width: 320px) and (max-device-width: 1024px) {
+		.work-wrap {
+			min-height: 60vh;
+			height: auto;
+		}
 	}
 </style>
