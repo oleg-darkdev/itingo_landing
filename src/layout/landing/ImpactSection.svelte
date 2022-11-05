c08285ca017db7e374bac943c9540d353080861b
DarkDev <darkdev_brand@riseup.net>
2022-11-05 23:27:22 +0200

    change text

--- src/layout/landing/ImpactSection.svelte
+++ src/layout/landing/ImpactSection.svelte
@@ -4,6 +4,6 @@
 
 
 <section class="pt-12 pb-20 bg-viol flex flex-col items-center " id="impact">
-	<h2 class="w-full m-8 text-center text-6xl text-lightYellow font-bold ">Już wykonane</h2>
+	<h2 class="w-full m-8 text-center text-6xl text-lightYellow font-bold ">Już mamy</h2>
 	<Impact />
 </section>
