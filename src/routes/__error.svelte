ad1bc5fc589c220bb8cc828d8f47569f6304872a
DarkDev <darkdev_brand@riseup.net>
2022-11-05 23:24:29 +0200

    fix for mobile

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,7 +1,13 @@
+<script>
+	import Header from "../layout/Header.svelte";
+</script>
+
+<Header />
+
 <div class="not-found w-full">
     <div style="height: 450px;" class="w-full flex flex-col items-center">
-	<h1 style="font-size: 300px;" class="m-0 p-0 text-center  text-lightYellow font-bold ">404</h1>
-    <p style="font-size: 90px; margin-top: -100px; width: 50%; min-width: 350px;" class="mb-3 text-center font-normal text-lightYellow">Nie znaleziono strony</p>
+	<h1 style="" class="title  text-lightYellow font-bold ">404</h1>
+    <p style="" class="mb-3 sub-title text-center font-normal text-lightYellow">Nie znaleziono strony</p>
     </div>
 </div>
 
@@ -15,5 +21,25 @@
 		background-size: 90%; 
 		background-attachment: fixed;
 		background-position: center;
+	}
+
+	.title {
+		font-size: 300px;
+		text-align: center;
+	}
+
+	.sub-title {
+		font-size: 90px; margin-top: -100px; width: 50%; min-width: 350px;
+	}
+
+	@media (min-device-width: 320px) and (max-device-width: 760px) {
+		.title {
+			font-size: 220px;
+			margin-bottom: 50px;
+		}
+
+		.sub-title {
+			font-size: 70px;  width: 50%; min-width: 350px;
+		}
 	}
 </style>
