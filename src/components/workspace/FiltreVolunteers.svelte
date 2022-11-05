b35b513f6cb42a6189ad069e76b94f8c2960e33a
DarkDev <darkdev_brand@riseup.net>
2022-11-05 02:11:53 +0200

    alfa v.

---
+++ src/components/workspace/FiltreVolunteers.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { Radio, Toggle  } from 'flowbite-svelte'
+</script>
+
+<div class="filtre w-5/12 flex-row flex p-2  bg-viol lg:rounded-xl md:rounded-xl justify-center content-center border-2 border-white bg-purple-800">
+    <div class="flex flex-col  mr-8">
+
+    <h2 class="text-3xl text-white font-black ">Filtr wolontariuszy</h2>
+
+
+    <label class="mt-1 text-sm font-medium block text-white flex items-center">
+    <input type="checkbox" value="" class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 mr-2 dark:bg-gray-700 dark:border-gray-600 sr-only peer rounded text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600">
+    <span class="mr-3 bg-gray-200 rounded-full peer-focus:ring-4 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:bg-white after:border-gray-300 after:border after:rounded-full after:transition-all dark:bg-gray-700 dark:border-gray-600 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 peer-checked:bg-blue-600 w-11 h-6 after:top-0.5 after:left-[2px] after:h-5 after:w-5 relative">
+    </span> Wolne od projektów
+    </label>
+    </div>
+
+    <div class="mt-2 flex flex-col">
+        <Radio name="example" checked={true}>kompetencji  </Radio>
+        <Radio name="example" checked={true}>aktywnośc</Radio>
+        <Radio name="example" checked={true}>ocena</Radio>
+        <!-- <label class="text-sm font-medium block text-white flex items-center">
+            <input type="radio" value="" name="ocena" class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 mr-2 dark:bg-gray-700 dark:border-gray-600 text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600">ocena
+        </label>
+        <label class="text-sm font-medium block text-white flex items-center">
+            <input type="radio" value="" name="kompetencji" class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 mr-2 dark:bg-gray-700 dark:border-gray-600 text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600">kompetencji 
+        </label>
+        <label class="text-sm font-medium block text-white flex items-center">
+            <input type="radio" value="" name="aktywnośc" class="w-4 h-4 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-2 mr-2 dark:bg-gray-700 dark:border-gray-600 text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600">aktywnośc
+        </label> -->
+    </div>
+</div>
+
+
+<style>
+    .filtre {
+        min-height: 100px;
+        height: auto;
+        position: relative;
+    }
+</style>
