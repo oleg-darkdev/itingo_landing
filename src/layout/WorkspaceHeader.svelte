3f93ea7775cb14e1a96859fd2f5319b1c4728d56
DarkDev <darkdev_brand@riseup.net>
2022-11-05 02:10:40 +0200

    alfa v.

---
+++ src/layout/WorkspaceHeader.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider, ButtonGroup, Button } from 'flowbite-svelte'
+</script>
+
+<Navbar class="bg-viol" let:hidden let:toggle>
+  <NavBrand href="/">
+    <img src="/images/logo.svg" class=" h-6 " alt="ITingo Logo"/>
+    <span class="self-center whitespace-nowrap lg:text-5xl md:text-4xl  text-3xl   text-lightYellow text-stantley">
+      ITingo
+    </span>
+  </NavBrand>
+
+  <NavUl {hidden} >
+    <NavLi href="/" active={true}>Strona główna</NavLi>
+    <NavLi href="/workspace/startups">Startupy</NavLi>
+    <NavLi href="/workspace/startups">NGO Projekty</NavLi>
+    <NavLi href="/workspace/volounteers">IT wolontariusze </NavLi>
+    <NavLi href="#faq">FAQ</NavLi>
+  </NavUl>
+ 
+    <div class="flex items-center md:order-2">
+    <!-- <Avatar id="avatar-menu" src="/images/oleg.png" /> -->
+    <NavHamburger on:click={toggle} class1="w-full md:flex md:w-auto md:order-1"/>
+  </div>
+  <div class="flex items-center md:order-2">
+    <ButtonGroup>
+      <Button href='/login' outline color="yellow" size="md">Sign In</Button>
+      <Button href='/signup' size="md" color="yellow">Sign up</Button>
+    </ButtonGroup>
+  </div>
+
+</Navbar>
--- src/layout/WorkspaceSection.svelte
+++ src/layout/WorkspaceSection.svelte
@@ -1,14 +1,16 @@
 <script>
-import { Button } from 'flowbite-svelte';
+import { Button, ButtonGroup } from 'flowbite-svelte';
 
 
 export let sectionData;
 </script>
 
 
-<section class="workspace flex justify-center lg:p-20 pt-4 pb-4">
-    <div class="lg:w-11/12 md:w-11/12 w-12/12 bg-viol lg:rounded-xl md:rounded-xl flex flex-col items-center lg:pr-4 lg:pl-4 pb-8" >
-        <h2 class="m-4 mb-2  text-6xl text-center text-lightYellow font-black">{sectionData.title}</h2>
+
+    <div class="lg:w-11/12 md:w-11/12 w-12/12 bg-viol lg:rounded-xl md:rounded-xl flex flex-col items-center pt-2 lg:pr-4 lg:pl-4 pb-8" >
+        <!-- <h2 class="m-4 mb-2  text-6xl text-center text-lightYellow font-black">{sectionData.title}</h2> -->
+        
+        
         <!-- <span class="mb-3 font-normal text-gray-700 text-center " >{sectionData.subTitle} {sectionData.howMuch}</span> -->
         <!-- <h3 class="m-4 mb-6  text-4xl text-lightYellow font-regular"></h3> -->
         
@@ -19,13 +21,20 @@ export let sectionData;
 
                 </slot>
         </div>
+                <slot name="filtre">
+
+                </slot>
 
             <slot name="all">
 
             </slot>
-        <Button href={sectionData.link} size="xl" color="yellow" class="w-6/12">All volunteers in this category</Button>
+        <ButtonGroup class="w-full flex justify-center ">
+            <Button size="xl" color="yellow" >Złóż wniosek o projekt</Button>
+            <Button href={sectionData.link}  size="xl" color="yellow" outline  class="">Wszyscy wolontariusze</Button>
+        </ButtonGroup>
+
     </div>
-</section>
+
 
 
 
