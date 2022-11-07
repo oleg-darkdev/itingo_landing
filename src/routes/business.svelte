5b5a00ede6cf6f3692cb80bf08ac6887688024f9
DarkDev <darkdev_brand@riseup.net>
2022-11-07 19:22:38 +0200

    alfa v.

---
+++ src/routes/business.svelte
@@ -0,0 +1,28 @@
+<script>
+	import BannerSection from "../layout/BannerSection.svelte";
+	import FaqSection from '../layout/FaqSection.svelte';
+	import Header from '../layout/Header.svelte';
+	import ContactSection from '../layout/ContactSection.svelte';
+	
+	
+	import faqList from '../data/faq/businessFaqList';
+
+</script>
+
+<svelte:head>
+	<title>ITingo dla biznesu</title>
+</svelte:head>
+
+<Header />
+
+
+<BannerSection bgImg='/images/business/banner_business-bg.svg'/>
+
+
+<FaqSection title='Pytania i odpowiedzi dla sponsorów i partnerów' {faqList} bgImgClass='faq-bg-partners'/>
+<ContactSection />
+
+
+<style>
+    
+</style>
