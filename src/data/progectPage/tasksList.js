805cc68898750db396eea83cf7feb5d64a5f5bf7
DarkDev <darkdev_brand@riseup.net>
2022-11-12 03:38:10 +0200

    add list tasks for project

---
+++ src/data/progectPage/tasksList.js
@@ -0,0 +1,24 @@
+import frontEndTask from './tasksTypes/frontEndTask';
+import backEndTask from './tasksTypes/backEndTask';
+import designTask from './tasksTypes/designTask';
+import computersTask from './tasksTypes/computersTask';
+import photoVideoTask from './tasksTypes/photoVideoTask';
+import projectingTask from './tasksTypes/projectingTask';
+import socialMediaTask from './tasksTypes/socialMediaTask';
+import seoTask from './tasksTypes/seoTask';
+import securityTask from './tasksTypes/securityTask';
+
+
+const tasksList = {
+	frontEndTask,
+	backEndTask,
+	designTask,
+	computersTask,
+	photoVideoTask,
+	socialMediaTask,
+	projectingTask,
+	seoTask,
+	securityTask
+};
+
+export default tasksList;
