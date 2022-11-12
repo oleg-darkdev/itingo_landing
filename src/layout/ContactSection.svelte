620b388066b2bdbaac26938f2b94164d693fcacd
DarkDev <darkdev_brand@riseup.net>
2022-11-12 18:17:41 +0200

    change link to faq page

--- src/layout/ContactSection.svelte
+++ src/layout/ContactSection.svelte
@@ -10,7 +10,7 @@
 		<p class="m-2 font-bold text-center w-8/12 leading-tight mb-2 text-white">
 			Prosimy! Zanim skontaktujesz się z nami, przestudiuj tęn   
 		
-			  <a href="/" class="inline-flex items-center text-lightYellow hover:underline">
+			  <a href="/faq" class="inline-flex items-center text-lightYellow hover:underline">
 				listę pytań i odpowiedzi. 
 				<!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" /></svg> -->
 			</a> 
