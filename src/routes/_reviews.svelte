dc48cbe181842ca7cb137b2e77b77ddcde0083f4
DarkDev <darkdev_brand@riseup.net>
2022-11-12 18:18:04 +0200

    proto for reviews page

---
+++ src/routes/_reviews.svelte
@@ -0,0 +1,21 @@
+<script>
+	import FaqSection from '../layout/FaqSection.svelte';
+	import ContactSection from '../layout/landing/ContactSection.svelte';
+	import faqList from '../data/faq/ngoFaqList';
+
+	
+</script>
+
+<svelte:head>
+	<title>Referencje o ITingo</title>
+</svelte:head>
+
+<!-- anchor: Feedback block for bussines -->
+
+<!-- anchor: Feedback block for ngo -->
+
+<!-- anchor: Feedback block for volunteers -->
+
+<FaqSection {faqList}/>
+
+<ContactSection />
