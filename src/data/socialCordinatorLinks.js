481d16fd6ec7d7623d8417fb9196c9e6b6ce1c03
DarkDev <darkdev_brand@riseup.net>
2022-11-17 11:47:09 +0200

    add fb & ln links

--- src/data/socialCordinatorLinks.js
+++ src/data/socialCordinatorLinks.js
@@ -1,8 +1,8 @@
 const links = {
-	linkedin: '',
+	linkedin: 'https://www.linkedin.com/in/oleg-darkdev?original_referer=https%3A%2F%2Flinktr.ee%2F',
 	github: 'https://github.com/oleg-darkdev/itingo',
 	twitter: '',
-	facebook: ''
+	facebook: 'https://www.facebook.com/hulio.mondre'
 };
 
 
