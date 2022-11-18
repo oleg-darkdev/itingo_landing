5b5f181a30dff1d388939e213b86f765837e9278
DarkDev <darkdev_brand@riseup.net>
2022-11-18 18:41:15 +0200

    add block: Support Us

---
+++ src/layout/SupportSection.svelte
@@ -0,0 +1,51 @@
+<script>
+    import { ButtonGroup, Button } from 'flowbite-svelte';
+
+    export let supportData;
+</script>
+
+
+<section id="donate" class="bg-violDark w-full p-12 flex flex-col items-center ">
+{#each supportData as supportEl}
+<div class="lg:w-10/12 border border-gray-200  rounded-lg p-6 m-4 bg-viol shadow-lg">
+<h2 class="text-4xl w-full font-semibold text-gray-100 mt-8 ">{supportEl.title}</h2>
+<p class="text-2xl  mb-4 text-gray-100 lg:w-8/12">{supportEl.description}</p>
+<div class="flex-row flex flex-wrap justify-center  mb-6">
+{#each supportEl.donateTypes as support}
+<div data-aos={support.animation}
+		data-aos-delay="100"
+		data-aos-duration="1400" 
+		data-aos-once="false"
+		data-aos-mirror="true"    
+    class="m-2 bg-lightYellow  rounded-lg border border-gray-200 dark:border-gray-700 shadow-md flex max-w-sm flex-col">
+    <img class="rounded-t-lg" src={support.img} alt=""> 
+    <div class="p-4 sm:p-6">
+        <div class="flex items-baseline text-violDark">
+            <span class="text-5xl font-extrabold tracking-tight">{support.value}</span>
+            <span class="text-3xl font-semibold">zł</span>
+        </div>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-violDark">{support.title}
+        </h5> 
+        <p class="mb-3 font-normal text-violDark  leading-tight">
+            {support.description}
+        </p> 
+        <Button size="md" color="purple"  href="/">Wspieraj</Button>
+    </div>
+</div>
+{/each}
+</div>
+
+<!-- <div class="w-full flex justify-end">
+<ButtonGroup>
+    <Button size="xl" color="yellow" outline  href="/">Wspieram jednorazowo</Button>
+    <Button size="xl" color="yellow"  href="/">Wspieram co miesiąc</Button>
+</ButtonGroup>
+</div> -->
+</div>
+{/each}
+</section>
+
+<style>
+
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,6 +11,122 @@
 	import dataForTimeline from '../data/ngo/dataForTimeline';
 	import faqList from '../data/faq/landingFaqList';
 	import FeedbackSection from "../layout/FeedbackSection.svelte";
+	import SupportSection from '../layout/SupportSection.svelte';
+
+
+	const supportNgo = 
+		{
+		title: 'Pomoc w realizacji stron www dla NGO',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		donateTypes: [
+			{
+				title: 'Hosting dla strony  na 1 miesiąc',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+				img: 'images/support/support_ngo_server.svg',
+				value: '22',
+				animation: 'fade-right'
+			},
+			{
+				title: 'Nazwa domeny na 1 rok',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+				img: 'images/support/support_ngo_domain.svg',
+				value: '45',
+				animation: 'fade-down'
+			},
+			{
+				title: 'Strona www',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+				img: 'images/support/support_ngo_www.svg',
+				value: '240',
+				animation: 'fade-left'
+			}
+		]
+	};
+
+	const supportVols = 	{
+		title: 'Pomoc IT wolontariuszom',
+		description: 'Zebrane pieniądze przeznaczamy na wynajem serwerów i stworzenie nowych funkcjonalności dla platformy ITingo, zakup oprogramowania open-source - co pomaga nam znacznie skuteczniej pomagać organizacjom pozarządowym oraz IT wolontariaty.',
+		donateTypes: [
+        {
+            title: 'Wynajem serwera na miesiąc dla ITingo',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            img: 'images/support/support_vols_edu.svg',
+            value: '40',
+				animation: 'fade-right'
+        },
+        {
+            title: 'Stworzenie nowych funkcji dla platformy ITingo',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            img: 'images/support/support_vols_food.svg',
+            value: '100',
+				animation: 'fade-down'
+        },
+        {
+            title: 'Zakupy oprogramowania typu open source',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            img: 'images/support/',
+            value: '200',
+				animation: 'fade-left'
+        }
+    ]
+	};
+	const supportCoordinator = 	{
+		title: 'Wspierać koordynatora projektu',
+		description: 'Założyciel ITingo to uchodźca polityczny z Białorusi, twórca wielu inicjatyw społecznych i po prostu niezły człowiek - potrzebujący osobistego wsparcia.',
+		donateTypes: [
+        {
+            title: 'Olegowi na kawkę',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            img: 'images/support/support_oleg_coffe.svg',
+            value: '15',
+				animation: 'fade-right'
+        },
+        {
+            title: 'Olegowi na obiad',
+            description: 'Oleg być może jest kreatywnym inżynierem, ale jego gotowanie jest okropne. Nakarm Olega - smacznym obiadem.',
+            img: 'images/support/support_oleg_eat.svg',
+            value: '50',
+				animation: 'fade-down'
+        },
+        {
+            title: 'Olegowi na butlę z gazem',
+            description: 'Oleg nie ma domu, nie może wrócić na Białoruś. Butle z gazem są jedynym środkiem do gotowania i ogrzewania przyczepy kempingowej, w której mieszka Oleg.',
+            img: 'images/support/support_oleg_gas.svg',
+            value: '100',
+				animation: 'fade-left'
+        }
+    ]
+};
+	
+	const supportITingo = 	{
+		title: 'Udostępnij IT wolontariuszom możliwość pomagania NGO',
+		description: 'Zebrane pieniądze przeznaczamy na wynajem serwerów i stworzenie nowych funkcjonalności dla platformy ITingo, zakup oprogramowania open-source - co pomaga nam znacznie skuteczniej pomagać organizacjom pozarządowym oraz IT wolontariaty.',
+		donateTypes: [
+        {
+            title: 'Wynajem serwera na miesiąc dla ITingo',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            img: 'images/support/support_itingo_server.svg',
+            value: '40',
+				animation: 'fade-right'
+        },
+        {
+            title: 'Stworzenie nowych funkcji dla platformy ITingo',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            img: 'images/support/support_itingo_code.svg',
+            value: '100',
+				animation: 'fade-down'
+        },
+        {
+            title: 'Zakupy oprogramowania typu open source',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            img: 'images/support/support_itingo_tools.svg',
+            value: '200',
+				animation: 'fade-left'
+        }
+
+    ]
+};
+
 </script>
 
 <Header />
@@ -19,24 +135,41 @@
 	<title>ITingo - home</title>
 </svelte:head>
 
-<BannerSection bgImg='/images/landing/bg/banner_main-bg.svg'/>
+<BannerSection bgImg='/images/landing/bg/banner_main-bg.svg'/> <!-- white -->
 
-<ClientsSection />
+<ClientsSection /> <!-- yellow -->
 
-<StepsSection text='Znajdz wykwalifikowanych IT wolontariuszy w 3 prostych krokach' {dataForTimeline}>
+<ImpactSection /> <!-- viol -->
+
+
+
+
+<!-- <StepsSection text='Znajdz wykwalifikowanych IT wolontariuszy w 3 prostych krokach' {dataForTimeline}>
 	<svelte:fragment slot="block">
 		<RegistrationBlock /> 
 	</svelte:fragment>
-</StepsSection>
+</StepsSection> -->
 
 
-<ImpactSection />
+<!-- Closses progects --> <!-- white -->
 
-<OurFamilySection />
+<SupportSection supportData={[supportCoordinator, supportNgo, supportITingo, supportVols,]}/> <!-- viol , -->
 
-<FeedbackSection />
+<!-- Presentation --> <!-- white -->
 
-<FaqSection title='Pytania i odpowiedzi: sprawy ogólne' {faqList} bgImgClass='faq-bg-main'/>
+
+<FeedbackSection /> <!-- yellow -->
+
+
+<!-- become us --> <!-- viol -->
+
+<!-- roadmap --> <!-- yellow -->
+
+
+<!-- del <OurFamilySection /> -->
+
+
+<FaqSection title='Pytania i odpowiedzi: sprawy ogólne' {faqList} bgImgClass='faq-bg-main'/> <!-- white -->
 
 <ContactSection />
 
