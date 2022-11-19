729000cdf6da12b88a74e3123a15c6c7eaba319d
DarkDev <darkdev_brand@riseup.net>
2022-11-19 17:34:34 +0200

    add trello board

--- src/layout/landing/ImpactSection.svelte
+++ src/layout/landing/ImpactSection.svelte
@@ -1,9 +1,11 @@
 <script>
     import Impact from '../../components/landing/Impact.svelte';
+	import TrelloBoardSection from '../workspace/TrelloBoardSection.svelte';
 </script>
 
 
 <section class="pt-12 pb-20 bg-viol flex flex-col items-center " id="impact">
 	<h2 class="w-full m-8 text-center text-6xl text-lightYellow font-bold ">Już mamy</h2>
 	<Impact />
+	<TrelloBoardSection boardLink='https://trello.com/b/2RHOp366.html'/>
 </section>
