15128649e961f2a803cae5c60c7f7cbb81ca95a6
DarkDev <darkdev_brand@riseup.net>
2022-11-21 00:59:46 +0200

    refactoring benefits list

---
+++ src/components/landing/ClientBenefitsList.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    export let client;
+</script>
+
+
+<div class="pl-5 flex flex-col justify-start">
+    <ul class=" text-violDark max-w-md list-disc list-inside space-y-1">
+        {#each client as listItem}
+            <li class="text-left">
+                <span class="font-semibold text-lightYellow">{listItem.colored[0]}
+                </span> {listItem.defaultColour[0]}
+                <span class="font-semibold text-lightYellow">{listItem.colored[1]}	
+                </span> {listItem.defaultColour[1]}					
+            </li> 
+        {/each}
+    </ul>
+</div> 
