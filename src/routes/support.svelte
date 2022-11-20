881f6fb5e3f04fa58a59292aef22c36158a22125
DarkDev <darkdev_brand@riseup.net>
2022-11-21 01:03:58 +0200

    alfa v.

---
+++ src/routes/support.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Header from '../layout/Header.svelte';
+	import FaqSection from '../layout/FaqSection.svelte';	
+	import ContactSection from '../layout/ContactSection.svelte';
+	import faqList from '../data/faq/ngoFaqList';
+	import SupportSection from '../layout/SupportSection.svelte';
+	import BannerSection from "../layout/BannerSection.svelte";
+</script>
+
+<svelte:head>
+	<title>Wspieraj ITIngo</title>
+</svelte:head>
+
+<Header />
+
+<BannerSection bgImg='/images/landing/bg/banner_main-bg.svg'/> 
+
+<SupportSection />
+<!-- add how to we help blocks -->
+<a id="faq" ></a>
+<FaqSection title='Pytania i odpowiedzi: sprawy ogólne' {faqList} bgImgClass='faq-bg-main'/> 
+
+<ContactSection />
