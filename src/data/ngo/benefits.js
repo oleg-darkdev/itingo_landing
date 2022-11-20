4ee080a4f9439f6e5efdf5d675863b2e64a172e5
DarkDev <darkdev_brand@riseup.net>
2022-11-21 01:04:58 +0200

    alfa v. benefits data for ngo

---
+++ src/data/ngo/benefits.js
@@ -0,0 +1,100 @@
+let benefits = [
+	{
+		bgImage: 'reccomandation_letter-bg',
+		title: 'Dowód',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+		align: 'start',
+		anchor: 'letter',
+		miniImg: 'offer_ngo_vols.svg',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: 'reccomandation_letter-bg',
+		title: 'Dowód',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+		align: 'start',
+		anchor: 'letter',
+		miniImg: 'offer_ngo_partners.svg',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	
+	{
+		bgImage: 'reccomandation_letter-bg',
+		title: 'Dowód',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+		align: 'start',
+		anchor: 'letter',
+		miniImg: 'offer_ngo_brain.svg',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: 'reccomandation_letter-bg',
+		title: 'Dowód',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+		align: 'start',
+		anchor: 'letter',
+		miniImg: 'offer_ngo_finance.svg',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	},
+	{
+		bgImage: 'reccomandation_letter-bg',
+		title: 'Dowód',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		],
+		align: 'start',
+		anchor: 'letter',
+		miniImg: 'offer_ngo_creative.svg',
+		dataAboutService: [
+			{
+				title: 'Presentation',
+				subTitle: '',
+				description: [
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+				]
+			}
+		]
+	}
+];
+
+export default benefits;
