b42c54bfa7920f80f0d4a1c4cd33bcf2c5531e30
DarkDev <darkdev_brand@riseup.net>
2022-11-20 03:41:50 +0200

    fix padding

--- src/layout/landing/WeDoForBlock.svelte
+++ src/layout/landing/WeDoForBlock.svelte
@@ -10,7 +10,7 @@
 		data-aos-duration="800" 
 		data-aos-once="true"
 		data-aos-mirror="true"
-    style="min-height: 260px; height: auto; margin-top: -180px;" class=" md:w-8/12 text-center z-2 flex lg:w-5/12 flex-col bg-viol shadow-md text-gray-500 dark:text-gray-400 lg:rounded-lg md:rounded-lg lg:p-4 p-2 text-center">
+    style="min-height: 260px; height: auto; margin-top: -180px;" class="p-2 md:w-8/12 text-center z-2 flex lg:w-5/12 flex-col bg-viol shadow-md text-gray-500 dark:text-gray-400 lg:rounded-lg md:rounded-lg lg:p-4 p-2 text-center">
     <h5 class="mb-2 text-3xl font-bold text-white">IT wsparcie dla wspierających</h5>
     <p class="mb-5 text-base text-white sm:text-lg dark:text-gray-400">
 		  <span class="text-2xl text-lightYellow font-black">ITingo</span> to platforma internetowa, która łączy IT , wolontariuszy, organizacje pozarządowe, biznes i start-upy - w celu dostarczenia projektów IT online.
