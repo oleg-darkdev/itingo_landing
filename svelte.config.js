509a764bdb841da0c99eb0231947f01496632ace
DarkDev <darkdev_brand@riseup.net>
2022-11-20 04:55:00 +0200

    migration for versel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({ out: 'production' })
+		adapter: adapter(),
 	}
 };
 
