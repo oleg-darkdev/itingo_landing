e5c015c7d4f48ecf576c02c2e92478c91e1a20bd
DarkDev <darkdev_brand@riseup.net>
2022-11-21 19:49:14 +0200

    alfa v.

--- src/routes/family.svelte
+++ src/routes/family.svelte
@@ -6,12 +6,19 @@
 	import partnersList from '../data/ngo/partnersList';
 	import faqList from '../data/faq/ngoFaqList';
 	import SupportSection from '../layout/SupportSection.svelte';
-	
+	import ClientsSection from '../layout/landing/ClientsSection.svelte';
+	import WeDoForBlock from '../components/WeDoForBlock.svelte';
 </script>
 
 <svelte:head>
 	<title>ITingo dla NGO</title>
 </svelte:head>
+
+<ClientsSection> <!-- yellow -->
+	<svelte:fragment slot="weDoFor">
+		<WeDoForBlock />
+	</svelte:fragment>
+</ClientsSection>
 
 <!-- 
     1. sponsors
